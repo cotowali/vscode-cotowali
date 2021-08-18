db288ec158f0b4d0e013fcd0a3acf727d20adacc
zakuro <z@kuro.red>
2021-08-19 02:39:40 +0900

    init extension.ts

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 zakuro <z@kuro.red>. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { ExtensionContext } from 'vscode'
+export function activate(_context: ExtensionContext): void {
+  // eslint-ignore
+}
