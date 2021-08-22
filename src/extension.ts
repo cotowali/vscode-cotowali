e9f3dc5c6fcb0d32fff276be9cdbdd4ab15220b3
zakuro <z@kuro.red>
2021-08-22 13:16:46 +0900

    check kuqi exists

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,28 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { ExtensionContext } from 'vscode'
+import { ExtensionContext, window, workspace } from 'vscode'
+import { existsSync } from 'fs'
 export function activate(_context: ExtensionContext): void {
-  // eslint-ignore
+  if (hasKuqi()) {
+    // TODO: enableKuqi()
+  }
+}
+
+function getConfig() {
+  return workspace.getConfiguration('cotowali')
+}
+
+function hasKuqi() {
+  const path = getConfig().get<string>('kuqi.path')
+  if (path === '') {
+    return false
+  }
+
+  if (!existsSync(path)) {
+    window.showErrorMessage('Kuqi is not found. Are you placing it in the specified path?')
+    return false
+  }
+
+  return true
 }
